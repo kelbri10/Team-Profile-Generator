3d095489be1f53b645d36881e206742de2831b3f
kelbri10 <kelbri10@gmail.com>
2020-09-13 18:40:35 -0400

    added employee class to employee.js

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,1 +1,25 @@
 // TODO: Write code to define and export the Employee class
+class Employee { 
+    constructor(role, name, employeeID, email){ 
+        this.role = role; 
+        this.name = name; 
+        this.employeeID = employeeID; 
+        this.email = email; 
+    }
+    
+    getName(){ 
+        return this.name; 
+    }
+
+    getID(){ 
+        return this.employeeID; 
+    }
+
+    getEmail(){ 
+        return this.email; 
+    }
+
+    getRole(){
+        return this.role; 
+    }
+}
