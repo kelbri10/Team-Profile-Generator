6311995dd50960450442175fbf8dfe3635104179
kelbri10 <kelbri10@gmail.com>
2020-09-15 14:57:46 -0400

    added output folder and team.html

--- Develop/app.js
+++ Develop/app.js
@@ -13,6 +13,28 @@ const render = require("./lib/htmlRenderer");
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
+
+// After the user has input all employees desired, call the `render` function (required
+// above) and pass in an array containing all employee objects; the `render` function will
+// generate and return a block of HTML including templated divs for each employee!
+
+// After you have your html, you're now ready to create an HTML file using the HTML
+// returned from the `render` function. Now write it to a file named `team.html` in the
+// `output` folder. You can use the variable `outputPath` above target this location.
+// Hint: you may need to check if the `output` folder exists and create it if it
+// does not.
+
+// HINT: each employee type (manager, engineer, or intern) has slightly different
+// information; write your code to ask different questions via inquirer depending on
+// employee type.
+
+// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
+// and Intern classes should all extend from a class named Employee; see the directions
+// for further information. Be sure to test out each class and verify it generates an
+// object with the correct structure and methods. This structure will be crucial in order
+// for the provided `render` function to work! ```
+
+//asks initial questions of user, then uses role to filter out to final question 
 const startInquiry = () => {
 
     const questions = [
@@ -51,6 +73,7 @@ const startInquiry = () => {
     }); 
 }
 
+//called if user is manager, new Manager object is created 
 const managerQuestion = employee => { 
 
     const questions = [
@@ -68,6 +91,7 @@ const managerQuestion = employee => {
     }); 
 } 
 
+//called if user is engineer, new Engineer object is created
 const engineerQuestion = employee => { 
     console.log('this person is a engineer'); 
 
@@ -85,6 +109,7 @@ const engineerQuestion = employee => {
     }); 
 }
 
+//called if user is intern, new Intern object is created
 const internQuestion = (employee) => { 
     const question = [
         {
@@ -102,22 +127,4 @@ const internQuestion = (employee) => {
 }
 
 startInquiry(); 
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
 
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
